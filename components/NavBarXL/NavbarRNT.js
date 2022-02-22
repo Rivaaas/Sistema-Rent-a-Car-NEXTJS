4cd7901ea09c62e90bca35ba74b4dc23df82ab60
Rivaaas <rivaaas.v@gmail.com>
2022-02-21 22:27:28 -0300

    build

--- components/NavBarXL/NavbarRNT.js
+++ components/NavBarXL/NavbarRNT.js
@@ -10,21 +10,20 @@ const NavbarRNT = () => {
     return (
 
         <>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a className="navbar-brand" href="/">Rent A Car!</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" href="/">Rent A Car!</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 
-                        <span class="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"></span>
 
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a className="nav-link active" aria-current="page" href='/Catalogue' >Catalogo</a>
-                            <a className="nav-link" href="Agreement">Contrato</a>
-                            <a className="nav-link" href="/VehicleReceipt" tabIndex="-1">Recibir Vehiculo</a>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <Link className="nav-link active" aria-current="page" href='/Catalogue' >Catalogo</Link>
+                            <Link className="nav-link" href="Agreement">Contrato</Link>
+                            <Link className="nav-link" href="/VehicleReceipt" tabIndex="-1">Recibir Vehiculo</Link>
 
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                         </div>
                     </div>
                 </div>
