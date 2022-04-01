a5bf1fbbe655e7fc13ee290578abab427184317b
Victor Rivas <33326699+Rivaaas@users.noreply.github.com>
2022-04-01 08:28:48 -0300

    Vista Log

--- components/LogIn/Log.js
+++ components/LogIn/Log.js
@@ -1,34 +1,41 @@
-import React from 'react'
+import React from "react";
 
-import { H2 } from '../../styles/pages/LogStyle';
-import { Form, Button, Row } from 'react-bootstrap'
+import { H2 } from "../../styles/pages/LogStyle";
+import { Form, Button, Row } from "react-bootstrap";
 
 const Log = () => {
-    return (
-        <>
-            <Row>
-                <H2>Iniciar Sesion</H2>
-                <div className='mb-5'>
-                    <Form className='mb-5 col-md-12'>
-                        <Form.Group>
-                            <Form.Label>Usuario</Form.Label>
-                            <Form.Control  type="text" />
-                        </Form.Group>
-                        <Form.Group className='mt-3'>
-                            <Form.Label>Contraseña</Form.Label>
-                            <Form.Control type="password" />
-                        </Form.Group>
-                        <a href="#">No recuerdo mi contraseña</a>
-                        <div>
-                            <Button variant="primary" type="submit" className='mt-4'>
-                                Entrar
-                            </Button>
-                        </div>
-                    </Form>
-                </div>
-            </Row>
-        </>
-    )
-}
+  return (
+    <>
+      <Row className="justify-content-center">
+     
+          {/* <div className="col-4"></div> */}
+          <div className="col-4 m-5">
+            <H2>Iniciar Sesion</H2>
+            <Form className="mb-5 col-md-12">
+              <Form.Group>
+                <Form.Label>Usuario</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group className="mt-3">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control type="password" />
+              </Form.Group>
+              <a href="#">No recuerdo mi contraseña</a>
+              <div>
+                <Button variant="dark" type="submit" className="mt-4">
+                  Entrar
+                </Button>
+                <hr></hr>
+                <a>
+                  No tienes cuenta? <a href="##">Registrate</a>{" "}
+                </a>
+              </div>
+            </Form>
+          </div>
+          {/* <div className="col-4"></div> */}
+      </Row>
+    </>
+  );
+};
 
-export default Log
+export default Log;
